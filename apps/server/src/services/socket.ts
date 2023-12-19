e294586a34b7468e3099831d2415332347b2b819
jackaakash4 <jackaakash4@gmail.com>
2023-12-20 00:01:08 +0545

    feat: setup nodeJS socket server

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -11,6 +11,20 @@ class SocketService{
     get io(){
         return this._io;
     }
+
+    public initListeners(){
+        const io = this._io;
+        console.log('Init socket listener');
+        io.on('connect', (socket)=>{
+            console.log(`New socket connected`, socket.id);
+
+            socket.on('event:messsage', async ({message}: {message: string})=>{
+                console.log('New message receive: ', message);                
+            })
+        })
+    }
+
+
 }
 
 export default SocketService;
